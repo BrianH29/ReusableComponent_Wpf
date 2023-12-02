8571f224854a3620be090475657959affbebf34d
Brian Hwang <brianhwang29@gmail.com>
2023-12-02 11:14:46 +0900

    Add project files.

---
+++ ReusableComponent_Wpf/Components/TierCard.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReusableComponent_Wpf.Components
+{
+    /// <summary>
+    /// Interaction logic for TierCard.xaml
+    /// </summary>
+    public partial class TierCard : UserControl
+    {
+        public object Title
+        {
+            get { return (object)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(object), typeof(TierCard), new PropertyMetadata(string.Empty));
+
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Description.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(TierCard), new PropertyMetadata(string.Empty));
+
+
+        public Brush Color
+        {
+            get { return (Brush)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Color.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(Brush), typeof(TierCard), new PropertyMetadata(Brushes.Transparent));
+
+        public static readonly RoutedEvent JoinClickEvent = EventManager.RegisterRoutedEvent(nameof(JoinClick), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TierCard));
+        //actual event for routedEvent
+        public event RoutedEventHandler JoinClick
+        {
+            add { AddHandler(JoinClickEvent, value); }
+            remove { RemoveHandler(JoinClickEvent, value); }
+        }
+
+
+
+        public TierCard()
+        {
+            InitializeComponent();
+        }
+
+        private void OnJoinClick(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(JoinClickEvent));
+        }
+    }
+}
---
+++ ReusableComponent_Wpf/Components/TierCardList.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReusableComponent_Wpf.Components
+{
+    /// <summary>
+    /// Interaction logic for TierCardList.xaml
+    /// </summary>
+    public partial class TierCardList : UserControl
+    {
+        public TierCardList()
+        {
+            InitializeComponent();
+        }
+        private void OnJoinBasicClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Successfully joined the Basic tier.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void OnJoinProClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Successfully joined the Pro tier.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void OnJoinEnterpriseClicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Successfully joined the Enterprise tier.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
